011e6ee689df50a78bf25b466745437ab171eaad
dominikdobos <dominik.dobos33@gmail.com>
2024-09-30 08:52:11 +0200

    static void fv()

--- src/gitbranchnb/GitbranchNb.java
+++ src/gitbranchnb/GitbranchNb.java
@@ -4,6 +4,11 @@ package gitbranchnb;
 public class GitbranchNb {
 
     public static void main(String[] args) {
+        fv();
+    }
+
+    private static void fv() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
